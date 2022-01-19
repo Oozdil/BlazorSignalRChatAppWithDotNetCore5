397b10e3c1633c6c081a32918ea710cd1725460a
orçun özdil <eczorcun@hotmail.com>
2022-01-19 15:16:14 +0300

    Add project files.

---
+++ BlazorSignalRChatAppWithDotNetCore5/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSignalRChatAppWithDotNetCore5.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage",user,message);
+        }
+    }
+}
